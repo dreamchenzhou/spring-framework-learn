af42547b32b3d0225f1dc45669d0c3b26cac4d88
chenyong <123456>
2020-04-06 10:07:28 +0800

    dd

--- src/main/java/com/example/springioc/demo/DemoApplication.java
+++ src/main/java/com/example/springioc/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class DemoApplication {
 
+    //testgit
     public static void main(String[] args) {
       ApplicationContext context= SpringApplication.run(DemoApplication.class, args);
       SpringContextUtils.setApplicationContext(context);
